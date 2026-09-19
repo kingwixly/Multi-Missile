1c50bf53d67f7572466a6a13c59c66659fd44342
i main magneto in rivals so im cool <144202971+kingwixly@users.noreply.github.com>
2026-09-19 14:00:58 -0700

    Bump plugin version to 1.1.3

--- Plugin.cs
+++ Plugin.cs
@@ -13,7 +13,7 @@ namespace JUSTJ7780.kingwixly.MultiMissilePatches
     {
         public const string PluginGuid = "com.justj7780.kingwixly.multimissilepatches";
         public const string PluginName = "Multi Missile Add on Pack";
-        public const string PluginVersion = "1.1.2";
+        public const string PluginVersion = "1.1.3";
 
         internal static ManualLogSource Log;
         private Harmony harmony;
